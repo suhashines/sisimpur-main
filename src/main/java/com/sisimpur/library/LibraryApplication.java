9a44633f2d7a5a4f1ccd2e848c947ba9bf04acba
Suhas Abdullah <suhasabdullah43@gmail.com>
2025-03-15 14:42:43 +0600

    api/v1/book/id works

--- src/main/java/com/sisimpur/library/LibraryApplication.java
+++ src/main/java/com/sisimpur/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.sisimpur.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.sisimpur.library.repository")  // <- Add this
 public class LibraryApplication {
 
 	public static void main(String[] args) {
